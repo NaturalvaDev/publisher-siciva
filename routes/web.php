1b7e4068c6ece760dbbb229420cf618b9a17755f
Msyarif17 <m.syarifsetiadi@gmail.com>
2024-06-02 12:30:00 +0700

    #

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\WhatsappApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix("whatsapp-publisher")->group(function(){
+    Route::post("queue",[WhatsappApiController::class,"setQueue"]);
+    Route::get("progress",[WhatsappApiController::class,"getProgress"]);
 });
